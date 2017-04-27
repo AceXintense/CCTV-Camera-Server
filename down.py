36a2af9015f71a9f8ae812e44c391769396466c9
Sam Grew <shanker1210@hotmail.co.uk>
2017-04-27 19:47:58 +0000

    Inital working commit

---
+++ down.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import sys
+import pantilthat
+
+angle = (sys.argv[1])
+pantilthat.tilt(int(angle))
---
+++ reset.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import pantilthat
+
+pantilthat.pan(0)
+pantilthat.tilt(0)
